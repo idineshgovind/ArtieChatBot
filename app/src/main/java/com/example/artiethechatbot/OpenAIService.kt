5da864553d4ed2c1f28de3bc9c299f3e7525e8f1
Dinesh G <49871841+idineshgovind@users.noreply.github.com>
2023-01-10 09:49:22 +0530

    Update OpenAIService.kt

--- app/src/main/java/com/example/artiethechatbot/OpenAIService.kt
+++ app/src/main/java/com/example/artiethechatbot/OpenAIService.kt
@@ -10,7 +10,7 @@ interface OpenAIService {
 
     @POST("v1/completions")
     fun generateText(
-        @Header("Authorization") apiKey: String = "Bearer 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",
+        @Header("Authorization") apiKey: String = "Bearer <YOUR API_KEY>",
         @Header("Content-Type") type : String = "application/json",
         @Body request: Map<String, String>
     ): Call<ResponseBody>
